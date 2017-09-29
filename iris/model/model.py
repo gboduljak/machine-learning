f40a0538a2f5d7956c8ceebb062c493e7b4658e1
Gabrijel Boduljak <gabrijel@dump.hr>
2017-09-29 16:31:57 +0200

    wrote decision boundary visualisation

--- iris/model/model.py
+++ iris/model/model.py
@@ -22,14 +22,28 @@ sns.FacetGrid(dataset, hue="Species", size=5).map(plt.scatter, "SepalLengthCm", 
 sns.FacetGrid(dataset, hue="Species", size=5).map(plt.scatter, "PetalLengthCm", "PetalWidthCm").add_legend()
 radviz(dataset.drop("Id", axis=1), "Species")
 
+classes = {
+   'Iris-setosa' : 0,
+   'Iris-virginica' : 1, 
+   'Iris-versicolor': 2
+}
+
+classIds = {
+    0 : 'Iris-setosa',
+    1 : 'Iris-virginica', 
+    2 : 'Iris-versicolor'
+}
+
 X = dataset.iloc[:, 1 : 5]
 y = dataset.iloc[:, 5]
+y = list(map(lambda classAsString : classes[classAsString], y))
 
 # Preprocess Dataset
 standardScaler = StandardScaler()
 X = standardScaler.fit_transform(X)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
 
 # Train KNN model with cross validation 
 possibleKNeighbors    = list(range(1, 50,2))
@@ -44,7 +58,7 @@ plt.figure()
 plt.title('KNN Cross Validation Scores')
 plt.xlabel('Number of neighbors K')
 plt.ylabel('Accuracy')
-plt.plot(k_parameters, cv_scores)
+plt.plot(possibleKNeighbors, crossValidationScores)
 plt.show()
 
 
@@ -74,21 +88,15 @@ svcModel.fit(X_train, y_train)
 
 # Evaluate and display model
 
-classIds = {
-   'Iris-setosa' : 0,
-   'Iris-virginica' : 1, 
-   'Iris-versicolor': 2
-}
-
 pca = PCA(n_components=2)
 pca.fit(X_train)
 
 X_display = pca.transform(X_train)
 y_display = y_train
-]
     
 svcModel = SVC(C = bestC, gamma = bestGamma)
 svcModel.fit(X_display, y_display)
+knnModel.fit(X_display, y_display)
 
 X1, X2 = np.meshgrid(
         np.arange(start = X_display[:, 0].min() - 1, stop = X_display[:, 0].max() + 1, step = 0.01),
@@ -109,18 +117,33 @@ for i,j in enumerate(np.unique(y_display)):
     plt.scatter(X_display[y_display == j, 0], 
                 X_display[y_display == j, 1], 
                 c = ListedColormap(('red', 'green', 'blue'))(i), 
-                label = j
+                label = classIds[j]
     )
     
-plt.title('SVC on train set')
-plt.xlabel('Sepal Length')
-plt.ylabel('Sepal')
+plt.xlabel('PCA1')
+plt.ylabel('PCA2')
 plt.legend()
 plt.show()
 
 
+X1, X2 = np.meshgrid(
+        np.arange(start = X_display[:, 0].min() - 1, stop = X_display[:, 0].max() + 1, step = 0.01),
+        np.arange(start = X_display[:, 1].min() - 1, stop = X_display[:, 1].max() + 1, step = 0.01)
+)
 
+plt.contourf(X1, 
+             X2, 
+             knnModel.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), 
+             alpha = 0.5, 
+             cmap = ListedColormap(('red', 'green', 'blue'))
+)
 
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
 
-
-
+for i,j in enumerate(np.unique(y_display)):
+    plt.scatter(X_display[y_display == j, 0], 
+                X_display[y_display == j, 1], 
+                c = ListedColormap(('red', 'green', 'blue'))(i), 
+                label = classIds[j]
+    )
